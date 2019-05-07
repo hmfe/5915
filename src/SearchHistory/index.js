85df34052655690405be1cdafde6e50cc9a9abd9
wemstad <christian.wemtad@gmail.com>
2019-05-07 11:08:52 +0200

    style and format history

--- src/SearchHistory/index.js
+++ src/SearchHistory/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import './styles.css'
 
+const dateFormatter = new Intl.DateTimeFormat('sv-SE', {
+  year: 'numeric', month: 'numeric', day: 'numeric',
+  hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true
+})
+
 const SearchHistory = ({ history, onClearHistory, onRemoveItem }) => (
   <div className="searchHistory"><h2>Search History</h2> <button className="clearHistory" onClick={onClearHistory}>Clear search history</button>
-    <ul className="list">
+    <ul className="historyItems">
       {history.map((item) => (
-        <li key={item.date}>{item.value} {item.date} <button aria-label="Remove" title="Remove" className="clearItem" onClick={onRemoveItem(item)}>X</button></li>
+        <li className="historyItem" key={item.date}>
+          <span className="historyItem-name">{item.value}</span>
+          <span>{dateFormatter.format(item.date)}</span>
+          <button aria-label="Remove" title="Remove" className="clearItem" onClick={onRemoveItem(item)}>
+            <svg height="20" width="20">
+              <path strokeWidth="1" stroke="#A5A5A5" d="M 5,5 L 15,15 M 15,5 L 5,15" /></svg>
+          </button>
+        </li>
       ))}
     </ul>
   </div>
